9b8c05be3c9fc80cadd25bf37fc523556f70a836
Anton Patsev <patsev.anton@gmail.com>
2025-04-28 19:27:06 +0600

    add s3

---
+++ data.tf
@@ -0,0 +1,2 @@
+# Получаем информацию о конфигурации клиента Yandex
+data "yandex_client_config" "client" {}
---
+++ s3.tf
@@ -0,0 +1,25 @@
+// Create SA
+resource "yandex_iam_service_account" "s3manager" {
+  folder_id = data.yandex_client_config.client.folder_id
+  name      = "tf-test-sa"
+}
+
+// Grant permissions
+resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
+  folder_id = data.yandex_client_config.client.folder_id
+  role      = "storage.editor"
+  member    = "serviceAccount:${yandex_iam_service_account.s3manager.id}"
+}
+
+// Create Static Access Keys
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.s3manager.id
+  description        = "static access key for object storage"
+}
+
+// Use keys to create bucket
+resource "yandex_storage_bucket" "test" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket     = "tf-test-bucket-for-s3manager"
+}
